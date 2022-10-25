2ce359ddfd185762cb7c82ff33c27dbebb4e1b59
Dixiekun <dixiekun.pacheco@gmail.com>
2022-10-25 19:10:48 +0800

    First commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background-color: #fff;
+        color: #333;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+        box-shadow: #434581 1px 2px 9px 2px;
+    }
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {FeedbackStore} from '../stores/stores'
+    import { scale, slide } from "svelte/transition";
+    import FeedbackItem from "./FeedbackItem.svelte";
+</script>
+
+<ul>
+    {#each $FeedbackStore as item (item.id) }
+   
+        <li in:scale out:slide>
+            <FeedbackItem {item} />
+        </li>
+  
+    {/each}
+</ul>
